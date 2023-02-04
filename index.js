cdeb300572f427be8be962b4b13ddd9254e2be46
Gunner <>
2023-02-04 18:28:14 -0300

    publishing

---
+++ index.js
@@ -0,0 +1,12 @@
+var menu = window.document.getElementById('one')
+menu.addEventListener('click', click)
+function click() {
+    one.style.background ='red'
+}
+
+
+var menu = window.document.getElementById('two')
+menu.addEventListener('click', click2)
+function click2() {
+    two.style.background ='yellow'
+}
